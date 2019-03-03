779c376274ac704fc9691817ded8156a2d9643ee
Raqbit <raqbit@outlook.com>
2019-03-03 12:17:35 +0100

    Update deps, update stats command

--- src/commands/stats.command.ts
+++ src/commands/stats.command.ts
@@ -13,39 +13,33 @@ class StatsCommand extends Command {
         this.firebase = bot.firebase;
     }
 
-    public execute(message: Discord.Message, args: string[]) {
+    public async execute(message: Discord.Message, args: string[]) {
 
-        this.firebase.fetchGlobalStats().then((globalStats) => {
-            if (!globalStats) {
-                globalStats = {};
-                globalStats.msgCount = 0;
-                globalStats.charCount = 0;
-            }
+        let globalStats = {
+            msgCount: 0,
+            charCount: 0,
+        }
 
-            if (!globalStats.msgCount) {
-                globalStats.msgCount = 0;
-            }
+        try {
+            const fetchedStats = await this.firebase.fetchGlobalStats();
 
-            if (!globalStats.charCount) {
-                globalStats.charCount = 0;
-            }
-
-            message.channel.send('', {
-                embed: new Discord.RichEmbed()
-                    .setTitle('Mancave-Statistics')
-                    .setDescription('**Global Stats:**\nMessages sent: ' +
-                        globalStats.msgCount + '\nCharacters sent: ' + globalStats.charCount + '\n')
-                    .setURL('https://mancave-statistics.firebaseapp.com')
-                    .setColor('#ffc800')
-                    .setThumbnail('https://mancave-statistics.firebaseapp.com/favicon/mstile-310x310.png')
-                    .setFooter('Click link for all statistics.'),
-            });
-
-        }, (error) => {
-            // Promise rejected
+            globalStats.msgCount = fetchedStats.msgCount ? fetchedStats.msgCount : 0;
+            globalStats.charCount = fetchedStats.charCount ? fetchedStats.charCount : 0;
+        } catch (error) {
             message.channel.send('Could not fetch global statistics, see console for error.');
             console.log(error);
             return;
+        }
+
+        await message.channel.send('', {
+            embed: new Discord.RichEmbed()
+                .setTitle('Mancave-Statistics')
+                .setDescription('**Global Stats:**\nMessages sent: ' +
+                    globalStats.msgCount + '\nCharacters sent: ' + globalStats.charCount + '\n')
+                .setURL('https://mancave-statistics.firebaseapp.com')
+                .setColor('#ffc800')
+                .setThumbnail('https://mancave-statistics.firebaseapp.com/favicon/mstile-310x310.png')
+                .setFooter('Click link for all statistics.'),
         });
     }
 }
